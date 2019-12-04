42a73b31d7a6b928bfc940db2303751b16d3bd22
webostap <webostap@users.noreply.github.com>
2019-12-05 06:06:56 +1000

    yes

--- main.js
+++ main.js
@@ -11,3 +11,34 @@ if ('serviceWorker' in navigator) {
         });
   });
 }
+
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent Chrome 76 and later from showing the mini-infobar
+  //e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  showInstallPromotion();
+});
+
+window.addEventListener('click', (e) => {
+  // hide our user interface that shows our A2HS button
+//  window.style.display = 'none';
+  // Show the prompt
+  deferredPrompt.prompt();
+  // Wait for the user to respond to the prompt
+  deferredPrompt.userChoice
+    .then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted the A2HS prompt');
+      } else {
+        console.log('User dismissed the A2HS prompt');
+      }
+      deferredPrompt = null;
+    });
+});
+window.addEventListener('appinstalled', (evt) => {
+  console.log('a2hs installed');
+});
+
