fcb48bdac570720af64a56a51a9d5b7597aceb65
webostap <webostap@users.noreply.github.com>
2019-12-05 04:54:04 +1000

    sw

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ opd();
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('./service-worker.js')
         .then((reg) => {
           console.log('Service worker registered.', reg);
         });
